304b6c8944abea0e8b5d5c23cfe73aaa75d6a9e3
Mahmoud Howa <mahmoudhowa1@gmail.com>
2023-09-07 22:27:05 +0300

    Create table WorkingHours and add code model,controller,factory,grphql

--- database/factories/DistrictFactory.php
+++ database/factories/DistrictFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\City;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,10 @@ class DistrictFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name()
+            'name' => $this->faker->name(),
+            'city_id' => function(){
+                return City::factory()->create()->id;
+            },
         ];
     }
 }
